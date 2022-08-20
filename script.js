444fcf5468e56a64c5e555afe28ceda83c0b91de
Rafael <greifrafael@gmail.com>
2022-08-20 13:06:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// Importieren des Index (index.html)
+
+var firstName = Rafael, Felix;
+var age = 15/14;
+
+// Ausgaben in Fenster
+
+        alert("Hello, we are Rafael and Felix!");
+        alert("Our second website!");
+        alert("Join our discord!");
+
+// Frage 1 mit Input
+
+function frage1 {
+        let input = prompt('Wie viel ist 12 + 14?')
+                if (input === 26) {
+                        alert('Super, deine Antwort ist richtig!');
+                }
+
+                else {
+                        alert('Schade, ich stelle dir noch eine Frage!');
+                }
+}
+
+// Frage 2 mit Input
+
+function frage2 {
+        var ergebnis = false;
+        const ergebnis;
+
+        let input = prompt('So. Ist 2 zu 4 im Quadrat 8?');
+                if (input === false) {
+                        alert('Sehr gut überlegt!');
+                }
+
+                else {
+                        alert('Vielleicht beim nächsten Mal und jetzt noch viel Spaß auf unserer Webseite!');
+                }
+}
